4188ab97839d7e49ee204abf3839f00f10b9484b
Philipp Günther <philipp@matchwerk.de>
2019-09-20 21:59:00 +0200

    filtering

--- src/extensions.js
+++ src/extensions.js
@@ -3,7 +3,7 @@ const { flattenDeep, orderBy, map } = require('lodash')
 const tree = require('../data/home_m.json')
 const list = flattenDeep(tree)
 
-const printTopBy = mapFile => files => {
+const printTopBy = mapFile => (files, sort = 'size') => {
     const info = files.reduce((result, file) => {
         const key = mapFile(file)
         if(result[key]) {
@@ -14,12 +14,18 @@ const printTopBy = mapFile => files => {
         }
         return result
     }, {})
-    const infoList = map(info, ({ count, size }, key) => ({key, count, size: size / (1024 * 1024 * 1024) })).filter(({ size }) => !!size)
-    const top = orderBy(infoList, 'size', 'desc').slice(0, 9)
+    const infoList = map(info, ({ count, size }, key) => ({key, count, size: size / (1024 * 1024 * 1024) })).filter(({ size }) => !!size) 
+    const top = orderBy(infoList, [sort], ['desc']).slice(0, 9)
     
+    const sum = infoList.reduce((a, { size }) => a + size, 0)
+    const avg = sum / infoList.length
+
     console.log(`files: ${files.length}`)
-    console.log(`top`, top)
+    console.log(`top by ${sort}`, top)
     console.log(`top total size: ${top.reduce((total, { size }) => total + size, 0)}`)
+
+    console.log(`list total size: ${sum}`)
+    console.log(`avg size: ${avg}`)
 }
 
 const mapExtension = ({ name }) => name && name.includes('.') 
@@ -30,12 +36,32 @@ const printTopByExtension = printTopBy(mapExtension)
 
 const mapDate = ({ mtime }) => {
     const date = (new Date(mtime * 1000))
-    return isNaN(date) ? 'invalid' : `${date.getDay()}.${date.getMonth()}.${date.getFullYear()}`
+    return isNaN(date) ? 'invalid' : `${date.getFullYear()}-${date.getMonth().toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
 }
 
 const printTopByDate = printTopBy(mapDate)
 
-console.log(`by file extension`)
-printTopByExtension(list)
-console.log(`by date`)
-printTopByDate(list)
+const isYear = year => ({ mtime }) => {
+    const date = new Date(mtime * 1000)
+    if(isNaN(date)) return false
+    return year.getFullYear() === date.getFullYear()
+}
+
+const isMonth = month => ({ mtime }) => {
+    const date = new Date(mtime * 1000)
+    if(isNaN(date)) return false
+    return month.getFullYear() === date.getFullYear()
+        && month.getMonth() === date.getMonth()
+}
+
+const isDay = day => ({ mtime }) => {
+    const date = new Date(mtime * 1000)
+    if(isNaN(date)) return false
+    return day.getFullYear() === date.getFullYear()
+        && day.getMonth() === date.getMonth()
+        && day.getDate() === date.getDate()
+}
+
+const filteredList = list.filter(isMonth(new Date(2019, 7, 1)))
+
+printTopByDate(filteredList, 'key')
