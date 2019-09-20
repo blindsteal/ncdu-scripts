f7baf223562e9124992f5d0c2cd388d5e5ff550b
Philipp Günther <philipp@matchwerk.de>
2019-09-20 20:53:32 +0200

    by date

--- src/extensions.js
+++ src/extensions.js
@@ -3,21 +3,39 @@ const { flattenDeep, orderBy, map } = require('lodash')
 const tree = require('../data/home_m.json')
 const list = flattenDeep(tree)
 
-const info = list.reduce((result, { name, dsize }) => {
-    const ext = name && name.includes('.') 
-        ? name.slice(name.lastIndexOf('.')).toLowerCase()
-        : 'directory'
-    if(result[ext]) {
-        if(Number.isInteger(dsize)) result[ext].size += dsize
-        result[ext].count++
-    } else {
-        result[ext] = { count: 1, size: dsize }
-    }
-    return result
-}, {})
-const infoList = map(info, ({ count, size }, ext) => ({ext, count, size: (size / 1024) / 1024 / 1024 })).filter(({ size }) => !!size)
-const top = orderBy(infoList, 'size', 'desc').slice(0, 9)
+const printTopBy = mapFile => files => {
+    const info = files.reduce((result, file) => {
+        const key = mapFile(file)
+        if(result[key]) {
+            if(Number.isInteger(file.dsize)) result[key].size += file.dsize
+            result[key].count++
+        } else {
+            result[key] = { count: 1, size: file.dsize }
+        }
+        return result
+    }, {})
+    const infoList = map(info, ({ count, size }, key) => ({key, count, size: size / (1024 * 1024 * 1024) })).filter(({ size }) => !!size)
+    const top = orderBy(infoList, 'size', 'desc').slice(0, 9)
+    
+    console.log(`files: ${files.length}`)
+    console.log(`top`, top)
+    console.log(`top total size: ${top.reduce((total, { size }) => total + size, 0)}`)
+}
 
-console.log(`files: ${list.length}`)
-console.log(`top extensions`, top)
-console.log(`top extensions total: ${top.reduce((total, { size }) => total + size, 0)}`)
+const mapExtension = ({ name }) => name && name.includes('.') 
+    ? name.slice(name.lastIndexOf('.')).toLowerCase()
+    : 'directory'
+
+const printTopByExtension = printTopBy(mapExtension)
+
+const mapDate = ({ mtime }) => {
+    const date = (new Date(mtime * 1000))
+    return isNaN(date) ? 'invalid' : `${date.getDay()}.${date.getMonth()}.${date.getFullYear()}`
+}
+
+const printTopByDate = printTopBy(mapDate)
+
+console.log(`by file extension`)
+printTopByExtension(list)
+console.log(`by date`)
+printTopByDate(list)
